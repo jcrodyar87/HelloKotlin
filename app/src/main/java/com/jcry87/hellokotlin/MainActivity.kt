06899aba7b9918fd826fd2f7e81fd0359e2e59b1
Juan Carlos Rodriguez Yarmas <jcry87@gmail.com>
2023-01-01 11:02:37 -0500

    Add configuration texttospeech

--- app/src/main/java/com/jcry87/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/jcry87/hellokotlin/MainActivity.kt
@@ -2,10 +2,28 @@ package com.jcry87.hellokotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener{
+    var tts: TextToSpeech? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tts = TextToSpeech(this,this)
+
+        var message:String = findViewById<TextView>(R.id.tvHello).text.toString()
+    }
+
+    
+
+    override fun onInit(status: Int) {
+        if( status == TextToSpeech.SUCCESS)
+        {
+            findViewById<TextView>(R.id.tvHello).text = "Ready!"
+        }else{
+            findViewById<TextView>(R.id.tvHello).text = "Not available!"
+        }
     }
 }
